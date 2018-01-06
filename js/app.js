ffc1aec4b99af64701c7b7069e8c13b251d25885
Kevin MacPherson <kevin.macpherson@gmail.com>
2018-01-06 12:59:14 -0500

    Before refactor

--- js/app.js
+++ js/app.js
@@ -1,5 +1,15 @@
 // define cards here, repeated twice to get 16 elements
 let cards = ["dog", "cat", "mouse", "fish", "bird", "snake", "lizard", "spider", "dog", "cat", "mouse", "fish", "bird", "snake", "lizard", "spider"];
+// For tracking moves
+let moves = 0;
+// For tracking clicks.
+let clicks = 0;
+// For keeping track if the first click ID.
+let firstClick = null;
+// Tracking all successful clicks.
+let successfulClicks = [];
+//Get the list of cards in memory.
+let dispCards = document.querySelectorAll(".card");
 
 // Shuffle the cards to randomize them. Found on Stackoverflow
 let shuffleCards = function (array) {
@@ -11,13 +21,42 @@ let shuffleCards = function (array) {
   }
 };
 
-let placeCards = function () {
-  let displayCards = document.querySelectorAll(".card");
-  for (let i = 0; i < cards.length; i++ ) {
-    displayCards[i].textContent = cards[i];
+// Function for click event.
+let clickCard = function (evt) {
+  let cardId = evt.target.getAttribute('id');
+  if (!successfulClicks.includes(cardId)) {
+    if (clicks === 0 ) {
+      firstClick = cardId;
+      dispCards[cardId].textContent = cards[cardId];
+      clicks++;
+    } else if ( clicks === 1 ) {
+      clicks++;
+      if (cardId != firstClick) {
+        clicks = 0;
+        dispCards[cardId].textContent = cards[cardId];
+        setTimeout(function () {
+          if (dispCards[firstClick].textContent !== dispCards[cardId].textContent) {
+            dispCards[firstClick].textContent = '';
+            dispCards[cardId].textContent = '';
+          } else {
+            successfulClicks.push(firstClick);
+            successfulClicks.push(cardId);
+            if (successfulClicks.length === 16) {
+              console.log("you won");
+            }
+          }
+        }, 1000);
+        moves++;
+        console.log(moves);
+      } else if ( cardId === firstClick ) {
+        click--;
+      }
+    }
   }
-}
-
+};
 
 shuffleCards(cards);
-placeCards();
+
+for (let i = 0; i < dispCards.length; i++ ) {
+  dispCards[i].addEventListener('click', clickCard);
+};
